1ccc8fc40058a689210dcffbec42a154cac816fd
Zia Hassan <fowziahme@gmail.com>
2022-03-24 00:22:45 -0600

    updated Project GeoGames

---
+++ gamesSelection.js
@@ -0,0 +1,24 @@
+const gamesSelection = (req, res) => {
+    res.send(`<html>
+    <body><h1>Games:</h1>
+        <h4>Which game do you want to play?</h4><br/>
+        <form method="GET" action="/country">
+            <button type="submit">Country Game</button>
+        </form>
+        <form method="GET" action="/waterGames/oceanGame">
+            <button type="submit">Ocean Game</button>
+        </form>
+        <form method="GET" action="/waterGames/seaGame">
+            <button type="submit">Sea Game</button>
+        </form>
+        <form method="GET" action="/quit">
+            <button type="submit">Exit</button>
+        </form>
+    </body>
+    </html>`);
+};
+
+
+module.exports = {
+    gamesSelection,
+};
